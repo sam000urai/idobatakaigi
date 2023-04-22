7da7bbdbcfeccf4bfb7b26642c6f0f576f515075
sam000urai <sam000urai@gmail.com>
2023-04-22 12:21:28 +0900

    add

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 import CreateUser from './pages/CreateUser';
-import Login from './pages/Login';
+import Signin from './pages/Signin';
 import Room from './pages/Room';
 import Chat from './pages/Chat';
 import CreateRoom from './pages/CreateRoom'; // インポート追加
@@ -13,7 +13,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Login />} />
+        <Route exact path="/" element={<Signin />} />
         <Route path="/createUser" element={<CreateUser />} />
         <Route path="/room" element={<Room />} />
         <Route path="/createroom" element={<CreateRoom />} /> {/* ルート追加 */}
