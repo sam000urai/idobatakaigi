b108df11978d23fe3d90e2a362b816857674ffc3
sam000urai <sam000urai@gmail.com>
2023-04-29 10:45:17 +0900

    add

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,34 +1,15 @@
 import React from 'react'
 import Layout from '../components/Layout';
-import { useState } from 'react';
-import { useCollection } from 'react-firebase-hooks/firestore';
-import { firestore } from '../firebase';
 
-
-const Chat = ({ roomId }) => {
-    const [text, setText] = useState('');
-
-    const sendMessage = () => {
-        if (text.trim() === '') {
-            return;
-        }
-
-        firestore.collection('messages').add({
-            text,
-            roomId,
-            createdAt: firestore.FieldValue.serverTimestamp(),
-        });
-
-        setText('');
-    };
-
+const Chat = () => {
     return (
-        <div>
-            <h1>Room {roomId}</h1>
-            <textarea value={text} onChange={(e) => setText(e.target.value)} />
-            <button onClick={sendMessage}>Send</button>
-        </div>
-    );
-};
+        <Layout>
+            <h1>Room</h1>
+            chat
 
-export default Chat;
+
+        </Layout>
+    )
+}
+
+export default Chat
