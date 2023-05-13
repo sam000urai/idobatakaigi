d216baaa2427214743656e923c850c0b4e50c85c
sam000urai <sam000urai@gmail.com>
2023-05-13 12:07:29 +0900

    add

--- src/pages/CreateUser.js
+++ src/pages/CreateUser.js
@@ -5,58 +5,58 @@ import { Link, useNavigate } from 'react-router-dom';
 import { createUser } from '../plugins/firebase';
 
 const CreateUser = () => {
-  const [email, setEmail] = useState('');
-  const [displayName, setDisplayName] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const navigate = useNavigate();
-  const handleClick = async () => {
-    console.log('Email:', email);
-    console.log('Password:', password);
-    const result = await createUser(displayName, email, password);
-    console.log(
-      '🚀 ~ file: CreateUser.js ~ line 13 ~ handleClick ~ result',
-      result
-    );
-    if (result === 'success') {
-      navigate('/room');
-    } else {
-      setError('User作成に失敗しました。');
-    }
-  };
+    const [email, setEmail] = useState('');
+    const [displayName, setDisplayName] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+    const handleClick = async () => {
+        console.log('Email:', email);
+        console.log('Password:', password);
+        const result = await createUser(displayName, email, password);
+        console.log(
+            '🚀 ~ file: CreateUser.js ~ line 13 ~ handleClick ~ result',
+            result
+        );
+        if (result === 'success') {
+            navigate('/room');
+        } else {
+            setError('User作成に失敗しました。');
+        }
+    };
 
-  return (
-    <div>
-      <h1>CreateUser</h1>
-      <TextField
-        id="displayName"
-        label="displayName"
-        value={displayName}
-        onChange={(e) => setDisplayName(e.target.value)}
-      />
-      <br />
-      <TextField
-        id="email"
-        label="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <br />
-      <TextField
-        id="password"
-        label="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <br />
-      <Button variant="outlined" onClick={handleClick}>
-        Create
-      </Button>
-      <br />
-      <Link to="/">戻る</Link>
-      {error && <p>{error}</p>}
-    </div>
-  );
+    return (
+        <div>
+            <h1>CreateUser</h1>
+            <TextField
+                id="displayName"
+                label="displayName"
+                value={displayName}
+                onChange={(e) => setDisplayName(e.target.value)}
+            />
+            <br />
+            <TextField
+                id="email"
+                label="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <br />
+            <TextField
+                id="password"
+                label="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <br />
+            <Button variant="outlined" onClick={handleClick}>
+                Create
+            </Button>
+            <br />
+            <Link to="/">戻る</Link>
+            {error && <p>{error}</p>}
+        </div>
+    );
 };
 
 export default CreateUser;
